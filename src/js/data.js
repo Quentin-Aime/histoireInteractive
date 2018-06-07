3bdb234b29ec8c3068e26fdeb88ad8d34410a3a3
Quentin-Aime <quentin.aime.dev@gmail.com>
2018-06-07 11:29:16 +0200

    First commit

--- src/js/data.js
+++ src/js/data.js
@@ -23,29 +23,29 @@ var map = {
 		exitDest: [4, 5, 6], //id de la map
 		text: 'Vous vous êtes enfui. Devant se dresse des campagnes dont vous ne savez rien, avec deux sentiers, l\'un vous\
 		ramene dans la ville que vous venez de quitter sur votre gauche, l\'autre part sur votre droite. Vous pouvez également bravez\
-		\' l\'aventure et vous engouffrez dans cette campagne.',
+		 l\'aventure et vous engouffrez dans cette campagne.',
 		changes: '',
 		// pnj: [], // [x y sprite] on verra plus tard
 		functionsCalled: [] // function, [args]		
 	},
 	3: {
-		startingX: 780,
-		startingY: 300,
+		startingX: 100,
+		startingY: 130,
 		name: 'dungeon',
 		exits: [], //xmin, xmax, ymin, ymax
 		exitDest: [], //id de la map
-		text: '',
+		text: 'Vous êtes emmenez en prison, vous demandant ce que le sort vous réservera.',
 		changes: '',
 		// pnj: [], // [x y sprite] on verra plus tard
 		functionsCalled: ['theEnd'] // function, [args]		
 	},
 	4: {
-		startingX: 780,
-		startingY: 300,
+		startingX: 100,
+		startingY: 130,
 		name: 'dungeon',
 		exits: [], //xmin, xmax, ymin, ymax
 		exitDest: [], //id de la map
-		text: '',
+		text: 'Deux soldats vous rattrappent et se jettent sur vous. Vous êtes rapidement maîtrisé, et emmené en prison.',
 		changes: '',
 		// pnj: [], // [x y sprite] on verra plus tard
 		functionsCalled: ['theEnd'] // function, [args]		
@@ -53,23 +53,38 @@ var map = {
 	5: {
 		startingX: 780,
 		startingY: 300,
-		name: 'dungeon',
-		exits: [], //xmin, xmax, ymin, ymax
-		exitDest: [], //id de la map
-		text: '',
+		name: 'fields',
+		exits: [[300, 400, 335, 380]], //xmin, xmax, ymin, ymax
+		exitDest: [7], //id de la map
+		text: 'Il fait une chaleur implacable et vous êtes ébloui \
+		par les rayons du soleil brûlant. Avec soulagement, vous apercevez \
+		une oliveraie un peu plus loin.',
 		changes: '',
 		// pnj: [], // [x y sprite] on verra plus tard
-		functionsCalled: ['theEnd'] // function, [args]		
+		functionsCalled: [] // function, [args]		
 	},
 	6: {
 		startingX: 780,
 		startingY: 300,
+		name: 'fields',
+		exits: [[300, 400, 335, 380]], //xmin, xmax, ymin, ymax
+		exitDest: [7], //id de la map
+		text: 'Vous avez la nette impression de vous égarer. Sous l\'effet de la chaleur, \
+		vous ressentez le besoin de vous reposer quelques minutes. Vous apercevez quelques arbres à quelques pas.',
+		changes: '',
+		// pnj: [], // [x y sprite] on verra plus tard
+		functionsCalled: [] // function, [args]		
+	},
+	7: {
+		startingX: 100,
+		startingY: 130,
 		name: 'dungeon',
 		exits: [], //xmin, xmax, ymin, ymax
 		exitDest: [], //id de la map
-		text: '',
+		text: 'Vous vous reposez sous l\'ombre de la végétation, mais ce n\'est qu\'une fois emprisonné dans un filet que vous vous \
+		rendez compte que c\' était une ambuscade. Vous vous retrouvez en prison.',
 		changes: '',
 		// pnj: [], // [x y sprite] on verra plus tard
-		functionsCalled: ['theEnd'] // function, [args]		
-	},
+		functionsCalled: ['theEnd'] // function, [args]
+	}
 }
